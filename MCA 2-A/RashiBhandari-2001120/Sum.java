14ba6780299435fc21b82fc3266f516840102a47
RashiBhandari <85724849+RashiBhandari@users.noreply.github.com>
2021-06-18 13:49:29 +0530

    Add files via upload

---
+++ MCA 2-A/RashiBhandari-2001120/Sum.java
@@ -0,0 +1,13 @@
+public class Sum{
+
+    public static void main(String[] args) {
+
+       int n1=10,n2=10,n3=15;
+
+        int sum=n1+n2+n3;
+
+
+        System.out.println("sum = "+ sum);
+    }
+
+}
---
+++ MCA 2-A/RashiBhandari-2001120/fact.java
@@ -0,0 +1,10 @@
+class Fact{  
+ public static void main(String args[]){  
+  int i,fact=1;  
+  int n=8; 
+  for(i=1;i<=n;i++){    
+      fact=fact*i;    
+  }    
+  System.out.println(fact);    
+ }  
+}  
